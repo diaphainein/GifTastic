b286045b80bb189ee75499b3f910f61cafbc0a57
diaphainein <lauren.cotto@gmail.com>
2017-06-30 19:53:16 -0700

    Reworked JS logic to make GIFs appear

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,91 +1,199 @@
+
+
+//create button function
+function createButton(toons){
+  //make a button
+  var theTopic = toons;
+  var newButton = $("<button>");
+  //say what it is
+  newButton.html(theTopic);
+  newButton.attr("class", "topics btn btn-primary");
+  //assign data
+  newButton.attr("data-topic", theTopic);
+  //put it in there
+  $("#topics").append(newButton);
+}
+
+
+//create gifs function
+function createGif(input){
+  //clear current div
+  $("#gifs").html("");
+
+  // parse input into URL
+  var queryURL = "http://api.giphy.com/v1/gifs/search?q=" +
+    input + "&api_key=dc6zaTOxFJmzC&limit=10";
+
+  //ajax call: take input and generate 10 gifs based on input
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).done(function(response) {
+
+    var results = response.data;
+
+    //loop through ajax return data to generate gifs
+    for (var i = 0; i < results.length; i++) {
+      //make new div
+      var newGif = $("<div>");
+      //create rating
+      var p = $("<p>");
+      p.html("Rating: " + results[i].rating.toUpperCase());
+      var img = $("<img>"); 
+      img.attr("class", "gif");
+      //inject still url
+      img.attr("src", results[i].images.fixed_height_still.url);
+      //save still url
+      img.attr("data-still", results[i].images.fixed_height_still.url);
+      //save gif url
+      img.attr("data-anim", results[i].images.fixed_height.url);
+      //save state for animate function
+      img.attr("data-state", false);
+      newGif.append(p);
+      newGif.append(img);
+      $("#gifs").append(newGif);
+    }
+  });
+}
+
+
+//make gif moving or still
+function animate(thing){
+  //if aready still, animate. else, stop it.
+  var state = $(thing).data("state");
+    if(state==false){
+      $(thing).data("state", true);
+      $(thing).attr("src", $(thing).data("anim"));
+    }
+    else{
+      $(thing).data("state", false);
+      $(thing).attr("src", $(thing).data("still"));
+    }
+}
+
+//init
+//create button function
+function createButton(toons){
+  //make a button
+  var theTopic = toons;
+  var newButton = $("<button>");
+  //say what it is
+  newButton.html(theTopic);
+  newButton.attr("class", "topics btn btn-primary");
+  //assign data
+  newButton.attr("data-topic", theTopic);
+  //put it in there
+  $("#topics").append(newButton);
+}
+
+
+//create gifs function
+function createGif(input){
+  //clear current div
+  $("#gifs").html("");
+
+  // parse input into URL
+  var queryURL = "http://api.giphy.com/v1/gifs/search?q=" +
+    input + "&api_key=dc6zaTOxFJmzC&limit=10";
+
+  //ajax call: take input and generate 10 gifs based on input
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).done(function(response) {
+
+    var results = response.data;
+
+    //loop through ajax return data to generate gifs
+    for (var i = 0; i < results.length; i++) {
+      //make new div
+      var newGif = $("<div>");
+      //create rating
+      var p = $("<p>");
+      p.html("Rating: " + results[i].rating.toUpperCase());
+      var img = $("<img>"); 
+      img.attr("class", "gif");
+      //inject still url
+      img.attr("src", results[i].images.fixed_height_still.url);
+      //save still url
+      img.attr("data-still", results[i].images.fixed_height_still.url);
+      //save gif url
+      img.attr("data-anim", results[i].images.fixed_height.url);
+      //save state for animate function
+      img.attr("data-state", false);
+      newGif.append(p);
+      newGif.append(img);
+      $("#gifs").append(newGif);
+    }
+  });
+}
+
+
+//make gif moving or still
+function animate(thing){
+  //if aready still, animate. else, stop it.
+  var state = $(thing).data("state");
+    if(state==false){
+      $(thing).data("state", true);
+      $(thing).attr("src", $(thing).data("anim"));
+    }
+    else{
+      $(thing).data("state", false);
+      $(thing).attr("src", $(thing).data("still"));
+    }
+}
+
+//init
 var toons = ["The Simpsons", "Bob's Burgers", "Family Guy", "Beavis and Butthead", "Rick and Morty", "Adventure Time", "Archer", "South Park"];
+function init(arr){
+  for(var i=0; i<arr.length; i++){
+    createButton(arr[i]);
+  }
+}
+init(toons);
 
-      // displayGifs function re-renders the HTML to display the appropriate content
-      function displayGifs() {
+//event listenerss
+//click on a button
+$("#topics").on("click", ".topics", function(){
+  createGif($(this).data("topic"));
+});
 
-        var toon = $(this).attr("data-toon");
-        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" +
-        toon + "&api_key=dc6zaTOxFJmzC&limit=10";
+//click on a gif
+$("#gifs").on("click", ".gif",function(){
+  animate(this);
+});
 
-        // Creating an AJAX call for the specific toon button being clicked
-        $.ajax({
-          url: queryURL,
-          method: "GET"
-        }).done(function(response) {
+//create new button
+$("#newButton").on("click", function(){
+  var newVal = $("#textbox").val();
+  if(newVal != ""){
+    toons.push(newVal);
+    createButton(newVal);
+  }
+})
+function init(arr){
+  for(var i=0; i<arr.length; i++){
+    createButton(arr[i]);
+  }
+}
+init(toons);
 
-          // Creating a div to hold the gifs
-          var toonDiv = $("<div class='gifs'>");
+//event listenerss
+//click on a button
+$("#topics").on("click", ".topics", function(){
+  createGif($(this).data("topic"));
+});
 
-          // Storing the rating data
-          var rating = response.rating;
+//click on a gif
+$("#gifs").on("click", ".gif",function(){
+  animate(this);
+});
 
-          // Creating an element to have the rating displayed
-          var para = $("<p>").text("Rating: " + rating);
-
-          // Displaying the rating
-          toonDiv.append(para);
-
-          // Putting the gifs above the previous gifs
-          $("#gif-view").prepend(toonDiv);
-        });
-
-      }
-
-      // Function for displaying movie data
-      function renderButtons() {
-
-        // Deleting the gifs prior to adding new gif buttons so we don't get repeat buttons
-        $("#buttons-view").empty();
-
-        // Looping through the array of cartoon gifs
-        for (var i = 0; i < toons.length; i++) {
-
-          // Then dynamicaly generating buttons for each toon in the array
-          var a = $("<button>");
-          // Adding a class of gif to our button
-          a.addClass("gif");
-          // Adding a data-attribute
-          a.attr("data-toon", toons[i]);
-          // Providing the initial button text
-          a.text(toons[i]);
-          // Adding the button to the buttons-view div
-          $("#buttons-view").append(a);
-        }
-      }
-
-    // This function handles events where a toon button is clicked
-      $("#add-toon").on("click", function(event) {
-        event.preventDefault();
-        // This line grabs the input from the textbox
-        var toon = $("#toon-input").val().trim();
-
-        // Adding toon from the textbox to our array
-        toons.push(toon);
-
-        // Calling renderButtons which handles the processing of our toons array
-        renderButtons();
-      });
-
-      // Adding a click event listener to all elements with a class of "toon"
-      $(document).on("click", ".toon", displayGifs);
-
-      // Calling the renderButtons function to display the intial buttons
-      renderButtons();
-
-      // handler for the still and animated actions
-      $(".gif").on("click", function() {
-        console.log(this);
-        var state = $(this).attr("data-state");
-
-        if (state == "still") {
-        $(this).attr("data-state", "animate");
-        $(this).attr("src", $(this).attr("data-animate"));
-        console.log(this);
-      }
-
-      else {
-        $(this).attr("data-state", "still");
-        $(this).attr("src", $(this).attr("data-still"));
-      }
-
-    });
+//create new button
+$("#newButton").on("click", function(){
+  var newVal = $("#textbox").val();
+  if(newVal != ""){
+    pokemon.push(newVal);
+    createButton(newVal);
+  }
+})
